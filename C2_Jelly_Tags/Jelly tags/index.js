fa173546b56e3862830f1e393cb05021b2e04dde
Rutuja Pawar <163704171+Rutucode@users.noreply.github.com>
2024-07-21 23:09:15 +0530

    Peerlist challenges
    Peerlist challenges 2,3

---
+++ C2_Jelly_Tags/Jelly tags/index.js
@@ -0,0 +1,30 @@
+let pillBtns = document.querySelectorAll(".pill");
+
+pillBtns.forEach((pillBtn) => {
+  // Add click event listener
+  pillBtn.addEventListener("click", () => {
+    // Toggle clicked class
+    pillBtn.classList.toggle("clicked");
+
+    if (!pillBtn.querySelector("img")) {
+      let imgHTML =
+        '<img src="Peerlist Icons.png" alt="Icon" style="padding-left: 8px;">';
+      pillBtn.querySelector("li").innerHTML = `${
+        pillBtn.querySelector("li").textContent
+      } ${imgHTML}`;
+    }
+  });
+
+  // Add double-click event listener
+  pillBtn.addEventListener("dblclick", () => {
+    console.log("dblclick");
+    let liElement = pillBtn.querySelector("li");
+
+    // Remove image
+    let text = liElement.textContent.replace(/<img[^>]*>/g, "").trim();
+    liElement.innerHTML = text;
+
+    // Remove clicked class
+    pillBtn.classList.remove("clicked");
+  });
+});
